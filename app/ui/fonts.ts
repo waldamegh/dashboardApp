807067f318e75f7904e7afd49bf3ede5214e08ef
unknown <waldamegh@gmail.com>
2024-07-31 14:50:02 +0300

    f

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { Lusitana} from 'next/font/google';
+ 
+const inter = Inter({ subsets: ['latin'] });
+const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
+export {inter, lusitana}
